aed42ab3ba7b18a642433a436d65ea1f15568a1c
Brian Mitchell <bman4789@gmail.com>
2019-12-14 14:37:40 -0600

    Add tests for each placeholder

--- tests/ReactPlaceholder.test.tsx
+++ tests/ReactPlaceholder.test.tsx
@@ -9,11 +9,66 @@ import { shallow } from 'enzyme';
 jest.useFakeTimers();
 
 describe('ReactPlaceholder', () => {
-
-  it('renders the placeholder when the content is not ready', () => {
+  it('renders the text placeholder when the content is not ready', () => {
     const content = <div>Some content still loading...</div>;
     const tree = shallow(
-      <ReactPlaceholder type='text' rows={2} ready={false}>
+      <ReactPlaceholder type="text" rows={2} ready={false}>
+        {content}
+      </ReactPlaceholder>
+    );
+    expect(tree.contains(content)).toBe(false);
+    expect(tree.getElements()).toMatchSnapshot();
+  });
+
+  it('renders the media placeholder when the content is not ready', () => {
+    const content = <div>Some content still loading...</div>;
+    const tree = shallow(
+      <ReactPlaceholder type="media" rows={2} ready={false}>
+        {content}
+      </ReactPlaceholder>
+    );
+    expect(tree.contains(content)).toBe(false);
+    expect(tree.getElements()).toMatchSnapshot();
+  });
+
+  it('renders the textRow placeholder when the content is not ready', () => {
+    const content = <div>Some content still loading...</div>;
+    const tree = shallow(
+      <ReactPlaceholder type="textRow" ready={false}>
+        {content}
+      </ReactPlaceholder>
+    );
+    expect(tree.contains(content)).toBe(false);
+    expect(tree.getElements()).toMatchSnapshot();
+  });
+
+  it('renders the rect placeholder when the content is not ready', () => {
+    const content = <div>Some content still loading...</div>;
+    const tree = shallow(
+      <ReactPlaceholder type="rect" ready={false}>
+        {content}
+      </ReactPlaceholder>
+    );
+    expect(tree.contains(content)).toBe(false);
+    expect(tree.getElements()).toMatchSnapshot();
+  });
+
+  it('renders the round placeholder when the content is not ready', () => {
+    const content = <div>Some content still loading...</div>;
+    const tree = shallow(
+      <ReactPlaceholder type="round" ready={false}>
+        {content}
+      </ReactPlaceholder>
+    );
+    expect(tree.contains(content)).toBe(false);
+    expect(tree.getElements()).toMatchSnapshot();
+  });
+
+  it('renders a custom placeholder when the content is not ready', () => {
+    const content = <div>Some ready content</div>;
+    const customPlaceholder = <div>Custom Placeholder</div>;
+    const tree = shallow(
+      <ReactPlaceholder ready={false} customPlaceholder={customPlaceholder}>
         {content}
       </ReactPlaceholder>
     );
@@ -24,12 +79,7 @@ describe('ReactPlaceholder', () => {
   it('renders the placeholder only after the specified delay', () => {
     const content = <div>Some content still loading...</div>;
     const tree = shallow(
-      <ReactPlaceholder
-        ready
-        type='text'
-        rows={2}
-        delay={10000}
-      >
+      <ReactPlaceholder ready type="text" rows={2} delay={10000}>
         {content}
       </ReactPlaceholder>
     );
@@ -43,37 +93,38 @@ describe('ReactPlaceholder', () => {
     expect(tree.getElements()).toMatchSnapshot();
   });
 
-  it('renders the content when it\'s ready', () => {
+  it("renders the content when it's ready", () => {
     const content = <div>Some ready content</div>;
     const tree = shallow(
-      <ReactPlaceholder type='text' rows={2} ready>
+      <ReactPlaceholder type="text" rows={2} ready>
         {content}
       </ReactPlaceholder>
     );
     expect(tree.contains(content)).toBe(true);
     expect(tree.getElements()).toMatchSnapshot();
   });
-
 });
 
 describe('build', () => {
-
   it('build script generates every needed file', () => {
-    execSync('npm run build', { stdio:[] })
+    execSync('npm run build', { stdio: [] });
 
     const libPath = path.resolve(__dirname, '../lib');
 
     const libFiles = fs.readdirSync(libPath);
 
-    const files = flatten(libFiles.map(fileInRoot => {
-      const filePath = path.resolve(libPath, fileInRoot);
-      if (fs.lstatSync(filePath).isDirectory()) {
-        return fs.readdirSync(filePath).map(fileInFolder => `${fileInRoot}/${fileInFolder}`);
-      }
-      return fileInRoot;
-    }));
+    const files = flatten(
+      libFiles.map(fileInRoot => {
+        const filePath = path.resolve(libPath, fileInRoot);
+        if (fs.lstatSync(filePath).isDirectory()) {
+          return fs
+            .readdirSync(filePath)
+            .map(fileInFolder => `${fileInRoot}/${fileInFolder}`);
+        }
+        return fileInRoot;
+      })
+    );
 
     expect(files).toMatchSnapshot();
-  })
-
-})
+  });
+});
---
+++ tests/placeholders.test.tsx
@@ -0,0 +1,159 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import {
+  MediaBlock,
+  RectShape,
+  RoundShape,
+  TextBlock,
+  TextRow
+} from '../src/placeholders';
+
+describe('placeholder', () => {
+  describe('MediaBlock', () => {
+    it('renders a MediaBlock', () => {
+      const tree = shallow(<MediaBlock rows={2} color="rebeccapurple" />);
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+
+    it('renders a MediaBlock with custom styles and classNames', () => {
+      const tree = shallow(
+        <MediaBlock
+          rows={2}
+          color="rebeccapurple"
+          className="test more-classes"
+          style={{
+            backgroundColor: 'hotpink',
+            display: 'block'
+          }}
+        />
+      );
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+  });
+
+  describe('RectShape', () => {
+    it('renders a RectShape', () => {
+      const tree = shallow(<RectShape />);
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+
+    it('renders a RectShape with custom styles, classNames, and color', () => {
+      const tree = shallow(
+        <RectShape
+          color="rebeccapurple"
+          className="test more-classes"
+          style={{
+            backgroundColor: 'hotpink',
+
+            height: '50%',
+            width: '50%',
+            marginRight: 5
+          }}
+        />
+      );
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+  });
+
+  describe('RoundShape', () => {
+    it('renders a RoundShape', () => {
+      const tree = shallow(<RoundShape color="rebeccapurple" />);
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+
+    it('renders a RoundShape with custom styles and classNames', () => {
+      const tree = shallow(
+        <RoundShape
+          color="rebeccapurple"
+          className="test more-classes"
+          style={{
+            backgroundColor: 'hotpink',
+            height: '50%',
+            width: '50%',
+            borderRadius: '2px'
+          }}
+        />
+      );
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+  });
+
+  describe('TextBlock', () => {
+    it('renders a TextBlock', () => {
+      const tree = shallow(<TextBlock rows={2} color="rebeccapurple" />);
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+
+    it('renders a TextBlock with custom lineSpacing and widths', () => {
+      const tree = shallow(
+        <TextBlock
+          rows={2}
+          color="rebeccapurple"
+          lineSpacing={1.5}
+          widths={[10, 12, 15, 13, 14, 11]}
+        />
+      );
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+
+    it('renders a TextBlock with custom styles and classNames', () => {
+      const tree = shallow(
+        <TextBlock
+          rows={2}
+          color="rebeccapurple"
+          className="test more-classes"
+          style={{
+            backgroundColor: 'hotpink',
+            width: '50%'
+          }}
+        />
+      );
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+  });
+
+  describe('TextRow', () => {
+    it('renders a TextRow', () => {
+      const tree = shallow(<TextRow color="rebeccapurple" />);
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+
+    it('renders a TextRow with custom maxHeight and lineSpacing', () => {
+      const tree = shallow(
+        <TextRow color="rebeccapurple" maxHeight={123} lineSpacing={'5rem'} />
+      );
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+
+    it('renders a TextRow with custom styles and classNames, overriding set maxHeight and lineSpacing', () => {
+      const tree = shallow(
+        <TextRow
+          color="rebeccapurple"
+          className="test more-classes"
+          maxHeight={123}
+          lineSpacing={'5rem'}
+          style={{
+            backgroundColor: 'hotpink',
+            height: '5em',
+            width: '50%',
+            marginTop: 2,
+            maxHeight: 50
+          }}
+        />
+      );
+
+      expect(tree.getElements()).toMatchSnapshot();
+    });
+  });
+});
