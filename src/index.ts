3b837ccfa92e3a29430061c3c9e545706d6cfef1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 11:28:47 +0800

    Delete src directory

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-import ReactPlaceholder from './ReactPlaceholder';
-export default ReactPlaceholder;
--- src/utils.ts
+++
@@ -1,2 +0,0 @@
-export const joinClassNames = (...classNames: (string | undefined)[]) =>
-  classNames.filter(c => c).join(' ');
