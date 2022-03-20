159b5c2b0463530dfc7dfb4c5a2495cfadd634fa
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 11:32:11 +0800

    Delete tests directory

--- tests/setup.js
+++
@@ -1,8 +0,0 @@
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0);
-};
-
-const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-Enzyme.configure({ adapter: new Adapter() });
